03199139e22b60d6d6e3830dc7a7685135b422c3
ZDLeaf <zach@zincldn.co.uk>
2020-06-15 09:31:39 +0100

    trying to include boost

--- Watch.h
+++ Watch.h
@@ -4,11 +4,14 @@
 #include <iostream>
 #include <filesystem>
 #include <string>
+//#include <boost/filesystem.hpp>
 
+//namespace b = boost::filesystem;
 namespace fs = std::filesystem;
 using namespace std;
 
 // c++17 std::filesystem
+// linux filesystem changes - inotify
 
 class Watch {
     private:
