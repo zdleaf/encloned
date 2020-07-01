3b82481bc3ba835dda0c56466280fd6618e24655
zdleaf <40774084+zdleaf@users.noreply.github.com>
2020-07-01 15:15:47 +0100

    S3: list objects in bucket

--- include/enclone/remote/S3.h
+++ include/enclone/remote/S3.h
@@ -1,13 +1,17 @@
 #ifndef S3_H
 #define S3_H
 
+#include <string>
 #include <aws/core/Aws.h>
 #include <aws/s3/S3Client.h>
 #include <aws/s3/model/Bucket.h>
+#include <aws/s3/model/ListObjectsRequest.h>
+#include <aws/s3/model/Object.h>
 
 class S3 {
     private:
         Aws::SDKOptions options;
+        const Aws::String BUCKET_NAME = "enclone";
 
     public:
         S3();
@@ -15,6 +19,7 @@ class S3 {
 
         void callAPI();
         bool listBuckets(Aws::S3::S3Client s3_client);
+        bool listObjects(Aws::S3::S3Client s3_client);
 
         void execThread();
 
--- src/remote/S3.cpp
+++ src/remote/S3.cpp
@@ -10,7 +10,7 @@ S3::S3()
 
 S3::~S3()
 {
-    
+
 }
 
 void S3::callAPI()
@@ -19,6 +19,7 @@ void S3::callAPI()
     {
         Aws::S3::S3Client s3_client;
         listBuckets(s3_client);
+        listObjects(s3_client);
     }
     Aws::ShutdownAPI(options);
 }
@@ -42,6 +43,29 @@ bool S3::listBuckets(Aws::S3::S3Client s3_client){
         std::cout << "ListBuckets error: "
                   << outcome.GetError().GetExceptionName() << std::endl
                   << outcome.GetError().GetMessage() << std::endl;
+        return false;
+    }
+}
+
+bool S3::listObjects(Aws::S3::S3Client s3_client){
+    Aws::S3::Model::ListObjectsRequest objects_request;
+    objects_request.WithBucket(BUCKET_NAME);
+
+    auto list_objects_outcome = s3_client.ListObjects(objects_request);
+
+    if (list_objects_outcome.IsSuccess()) {
+        Aws::Vector<Aws::S3::Model::Object> object_list =
+            list_objects_outcome.GetResult().GetContents();
+        std::cout << "Files on S3 bucket " << BUCKET_NAME << std::endl;
+        for (auto const &s3_object : object_list)
+        {
+            std::cout << "* " << s3_object.GetKey() << std::endl;
+        }
+        return true;
+    } else {
+        std::cout << "ListObjects error: " <<
+            list_objects_outcome.GetError().GetExceptionName() << " " <<
+            list_objects_outcome.GetError().GetMessage() << std::endl;
         return false;
     }
 }
