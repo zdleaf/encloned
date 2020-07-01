bab4bb5f6b6c1513db9b49c5c9435439d2bee7ea
zdleaf <40774084+zdleaf@users.noreply.github.com>
2020-07-01 14:57:41 +0100

    list buckets on AWS

--- include/enclone/remote/S3.h
+++ include/enclone/remote/S3.h
@@ -14,6 +14,9 @@ class S3 {
         ~S3();
 
         void callAPI();
+        bool listBuckets(Aws::S3::S3Client s3_client);
+
+        void execThread();
 
         // delete copy constructors - this class should not be copied
         S3(const S3&) = delete;
--- src/remote/S3.cpp
+++ src/remote/S3.cpp
@@ -1,18 +1,47 @@
 #include <enclone/remote/S3.h>
 
-S3::S3(){
+// example API calls - https://docs.aws.amazon.com/sdk-for-cpp/v1/developer-guide/examples-s3-objects.html
+
+S3::S3()
+{
     options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Info; // turn logging on using the default logger
     options.loggingOptions.defaultLogPrefix = "log/aws_sdk_";
 }
 
-S3::~S3(){
-
+S3::~S3()
+{
+    
 }
 
-void S3::callAPI(){
-   Aws::InitAPI(options);
-   {
-      // make your SDK calls here.
-   }
-   Aws::ShutdownAPI(options);
+void S3::callAPI()
+{
+    Aws::InitAPI(options);
+    {
+        Aws::S3::S3Client s3_client;
+        listBuckets(s3_client);
+    }
+    Aws::ShutdownAPI(options);
+}
+
+bool S3::listBuckets(Aws::S3::S3Client s3_client){
+    auto outcome = s3_client.ListBuckets();
+    if (outcome.IsSuccess())
+    {
+        std::cout << "List of available buckets:" << std::endl;
+
+        Aws::Vector<Aws::S3::Model::Bucket> bucket_list = outcome.GetResult().GetBuckets();
+
+        for (auto const &bucket : bucket_list)
+        {
+            std::cout << bucket.GetName() << std::endl;
+        }
+
+        std::cout << std::endl;
+        return true;
+    } else {
+        std::cout << "ListBuckets error: "
+                  << outcome.GetError().GetExceptionName() << std::endl
+                  << outcome.GetError().GetMessage() << std::endl;
+        return false;
+    }
 }
