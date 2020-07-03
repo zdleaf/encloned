946aa69ee7d7f6a259a376f1c1d672b2bef9bd44
zdleaf <40774084+zdleaf@users.noreply.github.com>
2020-07-03 09:33:29 +0100

    Remote.h stub

---
+++ include/enclone/remote/Remote.h
@@ -0,0 +1,20 @@
+#ifndef REMOTE_H
+#define REMOTE_H
+
+#include <iostream>
+#include <string>
+
+// stub class - this will be the middle man for handling multiple remotes at one time, e.g. multiple upload to different locations
+// needs to store which remotes are available and turned on for upload
+
+class Remote{
+    private:
+
+
+    public:
+        Remote();
+
+
+};
+
+#endif
